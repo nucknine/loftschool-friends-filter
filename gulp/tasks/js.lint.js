b7147774aaf67bc5a251d415a64ec458b3af15d7
nucknine <nucknine@gmail.com>
2018-05-29 17:05:31 +0300

    codding plus/cross buttons

--- gulp/tasks/js.lint.js
+++ gulp/tasks/js.lint.js
@@ -3,7 +3,17 @@
 module.exports = function() {
   $.gulp.task('js:lint', function() {
     return $.gulp.src($.path.app)
-      .pipe($.gp.eslint())
+      .pipe($.gp.eslint({
+        globals: [
+          'VK',
+          'jQuery',
+          '$'
+        ],
+        envs: [
+          'browser',
+          'es6'
+        ]
+      }))
       .pipe($.gp.eslint.format());
   })
 };
--- source/js/app.js
+++ source/js/app.js
@@ -1,11 +1,7 @@
-// (function() {
-// 'use strict';
+(function() {
+'use strict';
 
 // svg4everybody();
-
-// VK.init({
-//     apiId: 6487685
-// });
 
 VK.init({
     apiId: 6487685
@@ -37,36 +33,38 @@ function callAPI(method, params) {
     })
 }
 
-auth()
+auth()    
     .then(() => {
-        return callAPI('users.get', { name_case: 'gen' });
-    })
-    .then(([me]) => {
-        // const headerInfo = document.querySelector('#headerInfo');
-
-        // headerInfo.textContent = `Друзья на странице ${me.first_name} ${me.last_name}`;
-
         return callAPI('friends.get', { fields: 'city, country, photo_100' });
     })
-    .then(friends => {        
+    .then(friends => {
         const template = document.querySelector('#user-template').textContent;
         const render = Handlebars.compile(template);
         const html = render(friends);
-        const results = document.querySelector('#results');
+        const results = document.querySelector('#js-results');
 
         results.innerHTML = html;
     }).then(() => {
         let counter = 0;
         let currentDrag;        
 
-        // document.addEventListener('click', e => {
-        //     if (e.target.classList.contains('new-item')) {
-        //         const newItem = createItem();
-        //         const zone = getCurrentZone(e.target);
-
-        //         zone.insertBefore(newItem, zone.lastElementChild)
-        //     }            
-        // });
+        document.addEventListener('click', e => {
+            if (e.target.parentElement.classList.contains('icon-plus')) {                
+                const item = e.target.parentElement.parentElement;
+                const filterZone = document.getElementById('js-filter-zone');
+                filterZone.insertBefore(item, filterZone.lastElementChild);
+                e.target.setAttribute('xlink:href', '/assets/img/sprites/sprite.svg#times');
+                e.target.parentElement.classList.add('icon-times');
+                e.target.parentElement.classList.remove('icon-plus');
+            } else if (e.target.parentElement.classList.contains('icon-times')) {
+                e.target.setAttribute('xlink:href', '/assets/img/sprites/sprite.svg#plus');
+                const item = e.target.parentElement.parentElement;                              
+                const resultZone = document.getElementById('js-results');
+                resultZone.insertBefore(item, resultZone.lastElementChild);
+                e.target.parentElement.classList.add('icon-plus');
+                e.target.parentElement.classList.remove('icon-times');
+            }    
+        });
 
         document.addEventListener('dragstart', (e) => {
             const zone = getCurrentZone(e.target);
@@ -124,4 +122,4 @@ auth()
         }
     });
 
-// })();
+})();
